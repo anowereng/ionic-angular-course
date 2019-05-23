46ec0f64584911d19253bfc1484ae53a47fef2a5
Anwer Ullah <Anwer Ullah@DESKTOP-EB9HOQL>
2019-05-23 22:47:25 +0600

    detail-ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  {
-    path: 'recipes', children: [
+  { path: 'recipes', children: [
     { path: '', loadChildren: './receipes/receipes.module#ReceipesPageModule' },
-    { path: ':recipeId', loadChildren: './receipes/receipes-detail.module#ReceipesDetailPageModule'},
-      { path: 'detail', loadChildren: './receipes/detail/detail.module#DetailPageModule' }
+    { path: ':recipeId', loadChildren: './receipes/detail/detail.module#DetailPageModule' }
   ]
 }
  ];
--- src/app/receipes/detail/detail.page.ts
+++ src/app/receipes/detail/detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ReceipeService } from 'src/app/services/receipe.service';
+import { ReceipeItem } from 'src/app/_model/receipeItem';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private activatedRoute: ActivatedRoute, private receipeService: ReceipeService) { }
+  public loadedRecipe: ReceipeItem;
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramap => {
+      if (!paramap.has('recipeId')) {
+        return;
+      }
+      const recipeid = paramap.get('recipeId');
+      this.loadedRecipe = this.receipeService.getReceipe(recipeid);
+    });
   }
 
 }
